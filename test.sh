6a439e70f54c82263e8ca5fd94bd96b9ef85dc47
root <root@ip-172-26-5-171.ec2.internal>
2023-09-24 21:36:42 +0000

    agent scripts

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [ -f /etc/passwdhhhhhhhhkklk ]
+
+then
+  echo exist
+else
+echo missing
+fi
+
+
+if [ -d /tmp/bess1 ]
+then 
+ echo exist
+else
+mkdir /tmp/bess1
+echo new > /tmp/bess1/file
+cat /tmp/bess1/file
+fi
